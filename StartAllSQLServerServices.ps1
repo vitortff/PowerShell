06a74d6cb083bf4b4295ffd5c005876ff0fb8dd5
Vitor Fava <vitortff@gmail.com>
2025-06-10 11:34:44 -0300

    Stop and Start SQL Services

---
+++ StartAllSQLServerServices.ps1
@@ -0,0 +1,11 @@
+﻿# Obter todos os serviços relacionados ao SQL Server
+$services = Get-Service | Where-Object { $_.DisplayName -like "*SQL Server*" -or $_.Name -like "MSSQL*" }
+
+foreach ($service in $services) {
+    if ($service.Status -ne "Running") {
+        Write-Host "Iniciando serviço: $($service.Name) - $($service.DisplayName)"
+        Start-Service -Name $service.Name
+    } else {
+        Write-Host "Serviço já em execução: $($service.Name)"
+    }
+}
---
+++ StopAllSQLServerServices.ps1
@@ -0,0 +1,12 @@
+﻿# Obter todos os serviços relacionados ao SQL Server
+$services = Get-Service | Where-Object { $_.DisplayName -like "*SQL Server*" -or $_.Name -like "MSSQL*" }
+
+foreach ($service in $services) {
+    if ($service.Status -eq "Running") {
+        Write-Host "Parando serviço: $($service.Name) - $($service.DisplayName)"
+        Stop-Service -Name $service.Name -Force
+    } else {
+        Write-Host "Serviço já parado: $($service.Name)"
+    }
+}
+
